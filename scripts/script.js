d6837e42477184dbae84479ca424ff8d5c88c3f7
Monti junior <montiabouou@gmail.com>
2023-12-03 15:21:45 -0800

    Initial script.js commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let sketchContainer = document.querySelector('.sketch-container');
+let randomColor = document.querySelector('.random-color');
+let clearBtn = document.querySelector('.clear');
+let colors = document.querySelector('.colorValues');
+let inputBtn = document.querySelector('.input');
+let submitBtn = document.querySelector('.submit');
+let title = document.querySelector('.title');
+
+
+
+
