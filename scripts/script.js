f17bec770e73b38b36a666c9ea2a3a5f8b90586e
Monti junior <montiabouou@gmail.com>
2023-12-03 15:45:57 -0800

    fix: fixed the bug that prevented getRandomColor() from working

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,34 @@
 let sketchContainer = document.querySelector('.sketch-container');
-let randomColor = document.querySelector('.random-color');
+let randomColor = document.querySelector('.random');
 let clearBtn = document.querySelector('.clear');
 let colors = document.querySelector('.colorValues');
 let inputBtn = document.querySelector('.input');
 let submitBtn = document.querySelector('.submit');
 let title = document.querySelector('.title');
+
+
+//by default sketchContainer has default 16 X 16 grid items
+for (let i = 0; i < 257; i++) {
+    let sketchItems = document.createElement('div');
+    sketchItems.classList.add('sketch-item');
+    sketchContainer.appendChild(sketchItems);
+}
+
+//function: generates a random color
+function getRandomColor(number) {
+    let random = Math.floor(Math.random() * (number) + 1);
+    return random;
+}
+
+randomColor.addEventListener('click',getRainBow);
+
+function getRainBow() {
+    sketchContainer.addEventListener('mouseover',(e)=>{
+        let target = e.target;
+        let backgroundClr = `rgb(${getRandomColor(255)},${getRandomColor(255)},${getRandomColor(255)})`;
+        target.style.backgroundColor = backgroundClr;
+    });
+}
 
 
 
